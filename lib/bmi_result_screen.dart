7e81a5bf137339408c40519ea3e211f0308e0596
Hasnaa Mostafa <hasnaamostafa88@gmail.com>
2023-08-12 20:20:02 +0300

    Update bmi_result_screen.dart

--- lib/bmi_result_screen.dart
+++ lib/bmi_result_screen.dart
@@ -13,7 +13,7 @@ class BmiResult extends StatelessWidget {
 });
 
   @override
-  Widget build(BuildContext context) git {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
